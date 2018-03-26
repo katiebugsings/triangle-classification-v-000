0d5cee81cb4a780b071bd0b8c2df98f105907a75
Katie Cassell <kcassell944@gmail.com>
2018-03-26 20:45:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
 
   def validation
     raise TriangleError if(side_one <= 0 || side_two <= 0 || side_three <= 0)
-  raise TriangleError if(side_one > side_two + side_three || side_two > side_one + side_three || side_three > side_one + side_two)
+  raise TriangleError if(side_one < side_two + side_three || side_two < side_one + side_three || side_three < side_one + side_two)
 end
 
 
